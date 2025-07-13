55605fa9eaba029b038b1be5649e4dd096c6a428
Sonal Kalokhe <sonalkalokhe.89@gmail.com>
2025-07-13 15:35:19 +0530

    first abstraction changes

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,15 +1,17 @@
 import { test as setup, expect } from "@playwright/test";
+import { LoginPage } from "../lib/pages/login.page";
 
 setup("Create customer 01 auth", async ({ page, context }) => {
   const email = "customer@practicesoftwaretesting.com";
   const password = "welcome01";
   const customer01AuthFile = ".auth/customer01.json";
+  const loginPage = new LoginPage(page);
 
   await page.goto("https://practicesoftwaretesting.com/auth/login");
 
-  await page.getByTestId("email").fill(email);
-  await page.getByTestId("password").fill(password);
-  await page.getByTestId("login-submit").click();
+  await loginPage.emailInput.fill(email);
+  await loginPage.passwordInput.fill(password);
+  await loginPage.loginButton.click();
 
   await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
   await context.storageState({ path: customer01AuthFile });
