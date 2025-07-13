a8e74feadf6d327d50a8747226f3f5d2f00d405a
Sonal Kalokhe <sonalkalokhe.89@gmail.com>
2025-07-13 15:55:24 +0530

    test abstractions

--- lib/pages/login.page.ts
+++ lib/pages/login.page.ts
@@ -15,4 +15,9 @@ export class LoginPage {
   async goto() {
     await this.page.goto("https://practicesoftwaretesting.com/auth/login");
   }
+  async login(email: string, password: string) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
 }
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -7,11 +7,8 @@ setup("Create customer 01 auth", async ({ page, context }) => {
   const customer01AuthFile = ".auth/customer01.json";
   const loginPage = new LoginPage(page);
 
-  await page.goto("https://practicesoftwaretesting.com/auth/login");
-
-  await loginPage.emailInput.fill(email);
-  await loginPage.passwordInput.fill(password);
-  await loginPage.loginButton.click();
+  await loginPage.goto();
+  await loginPage.login(email, password);
 
   await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
   await context.storageState({ path: customer01AuthFile });
